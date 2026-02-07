ca88d48fcb076d9c798e64561824052409daef2e
Enti_CTU <tienhuynh8337@gmail.com>
2026-02-07 09:19:30 +0700

    first_commit

---
+++ Buoi_1/deg.c
@@ -0,0 +1,24 @@
+/*
+Cho cấu trúc dữ liệu đồ thị được khai báo sử dụng ma trận đỉnh - đỉnh như sau:
+
+typedef struct {
+    int A[100][100];
+    int n;
+} Graph;
+Giả sử đồ thị vô hướng, không chứa khuyên, không chứa đa cung
+
+Viết hàm int deg(Graph* G, int x) để tính bậc của đỉnh x.
+
+int deg(Graph* G, int x) {
+}
+*/
+
+int deg(Graph *G, int x){
+    int i;
+    int deg = 0;
+    for(i=1; i<=G->n; i++){
+        if(G->A[x][i] == 1) 
+            deg++;
+    }
+    return deg;
+}
